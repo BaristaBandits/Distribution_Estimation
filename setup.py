ea442d8f88e71d92d4a086d5ae3f9cbac82418f9
Swathi Narashiman <143377975+BaristaBandits@users.noreply.github.com>
2026-05-07 16:48:54 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup
 from Cython.Build import cythonize
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 setup(
-    ext_modules=cythonize("semantic_norm.pyx")
+    ext_modules=cythonize(
+        os.path.join(BASE_DIR, "semantic_norm.pyx")
+    )
 )
